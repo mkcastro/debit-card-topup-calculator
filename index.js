d09c4ee613ba8d5b96684d70b79747a57e9e72f6
Mike Castro <unknown>
2023-04-06 06:48:30 +0800

    refactor

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const readline = require('readline');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const prompt = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-rl.question('Enter your cash amount: ', cash => {
-    rl.question('Enter your debit card balance: ', debitCardBalance => {
-        rl.question('Enter your debit card maintaining balance: ', debitCardMaintainingBalance => {
-            rl.question('Enter your credit card bill: ', creditCardBill => {
-                rl.question('Enter the transfer fee: ', transferFee => {
-                    const shortfall = debitCardMaintainingBalance - debitCardBalance;
-                    const totalAmountToSend = parseFloat(shortfall) + parseFloat(creditCardBill) + parseFloat(transferFee);
+const missingFunds = (debitCardBalance, debitCardMaintainingBalance, creditCardBill, transferFee, cash) => {
+    const shortfall = debitCardMaintainingBalance - debitCardBalance;
+    const totalAmountToSend = parseFloat(shortfall) + parseFloat(creditCardBill) + parseFloat(transferFee);
+    if (totalAmountToSend <= parseFloat(cash)) {
+        return `You need to send ${totalAmountToSend.toFixed(2)} to debit card.`;
+    } else {
+        return `Insufficient funds. Cannot transfer ${totalAmountToSend.toFixed(2)} from cash to debit card.`;
+    }
+}
 
-                    if (totalAmountToSend <= parseFloat(cash)) {
-                        console.log(`You need to send ${totalAmountToSend.toFixed(2)} to debit card.`);
-                    } else {
-                        console.log(`Insufficient funds. Cannot transfer ${totalAmountToSend.toFixed(2)} from cash to debit card.`);
-                    }
-                    rl.close();
-                });
-            });
-        });
-    });
-});
+const processTopUp = async () => {
+    const cash = await new Promise(resolve => prompt.question('Enter your cash: ', resolve));
+    const debitCardBalance = await new Promise(resolve => prompt.question('Enter your debit card balance: ', resolve));
+    const debitCardMaintainingBalance = await new Promise(resolve => prompt.question('Enter your debit card maintaining balance: ', resolve));
+    const creditCardBill = await new Promise(resolve => prompt.question('Enter your credit card bill: ', resolve));
+    const transferFee = await new Promise(resolve => prompt.question('Enter the transfer fee: ', resolve));
+    const result = missingFunds(debitCardBalance, debitCardMaintainingBalance, creditCardBill, transferFee, cash);
+    console.log(result);
+    prompt.close();
+}
+
+processTopUp();
